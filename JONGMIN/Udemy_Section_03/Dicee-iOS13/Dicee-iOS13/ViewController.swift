6927480e67d0edc27151a173efc6424bcc93c39f
jongmin <asdfaa22@naver.com>
2022-02-19 19:46:50 +0900

    add files by jongmin

---
+++ JONGMIN/Udemy_Section_03/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //뷰 초기 로드 함수
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //앱 실행시 이미지 불러오기
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = UIImage(named: "DiceTwo")
+
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped.")
+        
+    
+//        diceImageView1.image = UIImage(named: "DiceFour")
+//        diceImageView2.image = UIImage(named: "DiceThree")
+        
+        let leftDiceNumber = Int.random(in: 0...5)
+        
+        diceImageView1.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][leftDiceNumber]
+        
+        diceImageView2.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][leftDiceNumber]
+    }
+    
+
+}
+
